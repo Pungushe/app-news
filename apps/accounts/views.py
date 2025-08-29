b5f8f29a4cc1876ce391190472c0b96aff55eb44
Pungushe <y.borozna81@internet.ru>
2025-08-29 20:22:43 +0300

    добавляем urls/views/serializers для accounts

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,104 @@
-from django.shortcuts import render
+from rest_framework import status, generics, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from django.contrib.auth import login
+
+from .models import User
+
+from .serializers import (
+    UserRegistrationSerializer, 
+    UserLoginSerializer, 
+    UserProfileSerializer, 
+    UserUpdateSerializer, 
+    ChangePasswordSerializer
+)
+
+
+class RegisterView(generics.GenericAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        
+        refresh = RefreshToken.for_user(user)
+        
+        return Response(
+            {
+                "user": UserProfileSerializer(user).data,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "message": "Пользователь успешно зарегистрирован",
+            },
+            status=status.HTTP_201_CREATED,
+        )
+class LoginView(generics.GenericAPIView):
+    serializer_class = UserLoginSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        
+        login(request, user)
+        
+        refresh = RefreshToken.for_user(user)
+        
+        return Response(
+            {
+                "user": UserProfileSerializer(user).data,
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "message": "Пользователь успешно вошел",
+            },
+            status=status.HTTP_200_OK,
+        )
+
+class ProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+    
+    def get_serializer_class(self):
+        if self.request.method in ["PUT", "PATCH"]:
+            return UserUpdateSerializer
+        return UserProfileSerializer
+
+class ChangePasswordView(generics.RetrieveUpdateAPIView):
+    serializer_class = ChangePasswordSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+    
+    def update(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        
+        return Response({
+            "message": "Пароль успешно изменен"}, 
+                        status=status.HTTP_200_OK
+        )
+        
+@api_view(["POST"])
+@permission_classes([permissions.IsAuthenticated])
+def logout_view(request):
+    try:
+        if refresh_token := request.data.get("refresh"):
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        return Response({
+            "message": "Вы успешно вышли"}, status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({
+            "error": 'Неверный токен'
+            }, status=status.HTTP_400_BAD_REQUEST)
